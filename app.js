24874df9fa5bb823343c6f48e08408922b257af4
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-07-07 22:57:28 +0900

    Update files

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ app.get('/', function(req, res) {
 app.use('/client', express.static(__dirname+'/client'));
 
 serv.listen(2000);
+console.log("Server started.");
+
+var SOCKET_LIST = {};
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket) {
+    socket.id = Math.random();
+    socket.x = 0;
+    socket.y = 0;
+    SOCKET_LIST[socket.id] = socket;
+
+    socket.emit('severMsg', {
+        msg:'hello', 
+    });
+    
+});
+
+setInterval(function() {
+
+},1000/25);
