4755db78d2b3cab80936991f5566c1ebe5577755
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-07-15 23:05:32 +0900

    Update files

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ var Entity = function() {
         self.x += self.spdX;
         self.y += self.spdY;
     }
+    self.getDistance = function(pt) {
+        return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.x-pt.y,2));
+    }
     return self;
 }
 
@@ -49,12 +52,18 @@ var Player = function(id) {
         super_update();
 
         if(self.pressingAttack) {
-            self.shootBullet(self.pressingAttack);
+            self.shootBullet(self.mouseAngle);
         }
-        // 4:00
+        /*
+        if(self.pressingAttack) {
+            for(var i=-3;i<3;i++) {
+                self.shootBullet(i * 10 +self.mouseAngle);
+            }
+        }
+        */
     }
     self.shootBullet = function(angle) {
-        var b = Bullet(angle);
+        var b = Bullet(self.id,angle);
         b.x = self.x;
         b.y = self.y;
     }
@@ -114,12 +123,12 @@ Player.update = function() {
     return pack;
 }
 
-var Bullet = function(angle) {
+var Bullet = function(parent,angle) {
     var self = Entity();
     self.id = Math.random();
     self.spdX = Math.cos(angle/180*Math.PI) * 10;
     self.spdY = Math.sin(angle/180*Math.PI) * 10;
-
+    self.parent = parent;
     self.timer = 0;
     self.toRemove = false;
     var super_update = self.update;
@@ -128,6 +137,14 @@ var Bullet = function(angle) {
             self.toRemove = true;
         }
         super_update();
+
+        for(var i in Player.list) {
+            var p = Player.list[i];
+            if(self.getDistance(p) < 32 && self.parent !== p.id) {
+                // handle collision. ex: hp--;
+                self.toRemove = true;
+            }
+        }
     }
     Bullet.list[self.id] = self;
     return self;
@@ -139,10 +156,14 @@ Bullet.update = function() {
     for(var i in Bullet.list) {
         var bullet = Bullet.list[i];
         bullet.update();
-        pack.push({
-            x:bullet.x,
-            y:bullet.y,
-        });
+        if(bullet.toRemove) {
+            delete Bullet.list[i];
+        } else {
+            pack.push({
+                x:bullet.x,
+                y:bullet.y,
+            });
+        }
     }
     return pack;
 }
