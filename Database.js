71c76ac969c13b4f3a0fba5e153d40783a829f7a
Lua00808 <3102oikawa713hayaki@gmail.com>
2020-08-16 22:52:27 +0900

    db のコレクションが更新されない

--- Database.js
+++ Database.js
@@ -1,9 +1,9 @@
 var USE_DB = true;
 var mongojs = USE_DB ? require("mongojs") : null;
-var db =  USE_DB ? mongojs('localhost:27017/myGame', ['account','progress']) : null;
+var db = USE_DB ? mongojs('localhost:27017/myGame', ['account','progress']) : null;
 
 // account: {username:string, password:string}
-// progress: {username:string, items:[{id:stirng,amount:number}]}
+// progress: {username:string, items:[{id:string,amount:number}]}
 
 Database = {};
 Database.isValidPassword = function(data,cb) {
@@ -35,7 +35,9 @@ Database.addUser = function(data,cb) {
         return cb();
     }
     db.account.insert({username:data.username,password:data.password},function(err) {
+        console.log('create user %s', data.username);
         Database.savePlayerProgress({username:data.username,items:[]},function() {
+            console.log("create user111");
             cb();
         });
     });
@@ -53,5 +55,6 @@ Database.savePlayerProgress = function(data,cb) {
     if(!USE_DB) {
         return cb();
     }
-    db.progress.update({username:data.username},data,{upsert:true},cb);
+    db.progress.update({username:data.username},data, true, cb);
+    console.log('save create user %s', data.username);
 }
